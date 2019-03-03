f50ad0e8a31fd9d233c953f638735185a00b1f28
Antti Antinoja <antti@fennosys.fi>
2019-03-03 11:23:29 +0800

    Added comments and one more example with cast.

--- int/1-overflow/int_ovrflw.c
+++ int/1-overflow/int_ovrflw.c
@@ -3,9 +3,13 @@
 
 int main(void){
 
-	printf("\"long g=n*1024;\"\n");
+	printf("Size of int32_t .: %lu bits\n", sizeof(int32_t)*8);
+	printf("Size of int .....: %lu bits\n", sizeof(int)*8);
+	printf("Size of long ....: %lu bits\n\n", sizeof(long)*8);
+
+	printf("\"long g=n*1024; /* Undefined behaviour!! */\"\n");
 	for (int32_t n=2097150; n<2097155; n++){
-		long g=n*1024;  /* This will overflow after 2 iterations */
+		long g=n*1024; /* Undefined behaviour!! */
 		printf("g: %li n: %i\n", g, n);
 	}
 
@@ -15,9 +19,41 @@ int main(void){
 		printf("g: %li n: %i\n", g, n);
 	}
 
-	printf("\nint %lu bits.\n", sizeof(int)*8);
-	printf("long int %lu bits.\n", sizeof(long)*8);
+	printf("\n\"long g=(long)n*1024;\"\n");
+	for (int32_t n=2097150; n<2097155; n++){
+		long g=(long)n*1024; /* Works as intended */
+		printf("g: %li n: %i\n", g, n);
+	}
 
 	return 0;
 }
 
+/*   Links:
+ *   + Fixed-width integers: https://en.wikipedia.org/wiki/C_data_types#Fixed-width_integer_types
+ *
+ *   Notes:
+ *   + tested with gcc 7.3:
+ *   ...
+ *   "long g=n*1024;"
+ *   g: 2147481600 n: 2097150
+ *   g: 2147482624 n: 2097151
+ *   g: -2147483648 n: 2097152
+ *   g: -2147482624 n: 2097153
+ *   g: -2147481600 n: 2097154
+ *   ...
+ *
+ *   + tested with gcc 8.2:
+ *   ...
+ *   "long g=n*1024;"
+ *   g: 2147481600 n: 2097150
+ *   g: 2147482624 n: 2097151
+ *   g: 2147483648 n: 2097152
+ *   g: 2147484672 n: 2097153
+ *   g: 2147485696 n: 2097154
+ *   ...
+ *
+ *   With current CFLAGS gcc 8.2 actually gives a worning:
+ *   int_ovrflw.c:8:11: warning: iteration 2 invokes undefined behavior [-Waggressive-loop-optimizations]
+ *
+ *   (C) Antti Antinoja, 2019
+ */
