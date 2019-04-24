f6e040b810326bea0dc814d884f02bc95d53ec2d
Antti Antinoja <antti@fennosys.fi>
2019-04-24 13:48:36 +0800

    primitive utf-8 support

--- lua/2-embedding/embed-lua.c
+++ lua/2-embedding/embed-lua.c
@@ -16,14 +16,30 @@
 #endif
 
 #define LUA_FILE "./lua.lua"
-# define TRUE 1
-# define FALSE 0
+#define TRUE 1
+#define FALSE 0
+
+#define U8BNEXT 0x80 /* 0y10000000 */
+			/* utf-8 mask: */
+#define U8B1_2M 0xe0 /* 0y11100000 */
+#define U8B1_3M 0xf0 /* 0y11110000 */
+#define U8B1_4M 0xf8 /* 0y11111000 */
+			/* utf-8 values for multibyte characters: */
+#define U8B1_2  0xc0 /* 0y11000000 */
+#define U8B1_3  0xe0 /* 0y11100000 */
+#define U8B1_4  0xf0 /* 0y11110000 */
 
 int file_stat_check(const char * fn);
 void lf_error_print(int load_stat, const char *fn);
+			/* Registered for LUA: */
 static int lua_sleep(lua_State *Ls);
 static int lua_string_print(lua_State *);
 static int lua_string_reverse(lua_State *);
+			/* Debug: */
+void print_string_bytes(const char *);
+void print_char_bits(char);
+			/* UTF-8: */
+int has_utf8_bytes(const char *, int);
 
 int main(void) {
 	
@@ -57,7 +73,7 @@ int main(void) {
 		return 1;
 	}
 #endif
-
+			/* Register C functions for LUA: */
 	lua_register(lua, "sleep", lua_sleep);
 	lua_register(lua, "string_print", lua_string_print);
 	lua_register(lua, "string_reverse", lua_string_reverse);
@@ -96,40 +112,84 @@ void lf_error_print(int load_stat, const char *fn) {
 	}
 }
 
-static int lua_sleep(lua_State *Ls){
+static int lua_sleep(lua_State *Ls) {
 	int msec  = luaL_checkinteger(Ls, 1);
 	poll(NULL, 0, msec);
 	return 1;
 }
 
-static int lua_string_print(lua_State *Ls){
+static int lua_string_print(lua_State *Ls) {
 	const char *str = luaL_checkstring(Ls, 1);
 	printf(str);
 	return 0;
 }
 
-static int lua_string_reverse(lua_State *Ls){
+static int lua_string_reverse(lua_State *Ls) {
 	const char *str = luaL_checkstring(Ls, 1);
 	size_t len = strlen(str);
-	size_t x = 0;
-	int add_nl = FALSE;
+	size_t n_pos = len - 1;
 	char *new_str = malloc( sizeof(char) * len + 1 );
-	if ( str[len-1] == '\n' ) {
-		len--;
-		add_nl = TRUE;
+			/* One needs to finish the string with NULL character in C: */
+	new_str[len] = '\0';
+			/* In case the original string ends with new line
+			 * we terminate the new string also with new line: */
+	if ( str[n_pos] == '\n' ) {
+		new_str[n_pos] = '\n'; n_pos--;
 	}
-	while ( len-- ) {
-		new_str[x] = str[len];
-		x++;
+			/* Set our goal (max) */
+	size_t max = n_pos;
+	size_t s_pos = 0;
+	while ( s_pos <= max ) {
+			/* We need to add 1 for length (n_pos is index): */
+		size_t utf_bytes = has_utf8_bytes(str+s_pos, n_pos+1);
+		if ( utf_bytes ) {
+			/* We are here because a variable width (utf-8) character was detected. */
+			size_t utf_pos = utf_bytes;
+			while( utf_pos-- ) {
+				new_str[n_pos] = str[s_pos+utf_pos];
+				n_pos--;
+			}
+			/* Advance s_pos by the amount the utf8 character consumed: */
+			s_pos = s_pos + utf_bytes;
+		} else {
+			new_str[n_pos] = str[s_pos];
+			n_pos--;
+			s_pos++;
+		}
 	}
-	if ( add_nl ) {
-		new_str[x] = '\n';
-		x++;
-	}
-	new_str[x]='\0';
 	lua_pushstring(Ls, new_str);
 	free(new_str);
 	return 1;
+}
+
+void print_char_bits(char c){
+	for (int b=7; b>=0; b--) {
+		if ( c & ( 1 << b ) ) printf("1"); else printf("0");
+	}
+}
+
+void print_string_bytes(const char *s){
+	int len=strlen(s);
+	for (int n=0; n<len; n++) {
+		printf("s[%i]: ", n);
+		print_char_bits(s[n]);
+		printf("- %c\n", s[n]);
+	}
+}
+
+int has_utf8_bytes(const char *s, int len){
+			/* Keep compiler silent since we
+			 * don't have any validations on
+			 * place yet: */
+	len=len;
+			/* We have a multibyte character? */
+	if ( s[0] & U8BNEXT ) {
+			/* Check the lenth of the multibyte character: */
+		if ( (s[0] & U8B1_2M) == U8B1_2 ) return 2;
+		if ( (s[0] & U8B1_3M) == U8B1_3 ) return 3;
+		if ( (s[0] & U8B1_4M) == U8B1_4 ) return 4;
+	}
+	return 0;
 }
 
 /*   Check:
@@ -138,6 +198,7 @@ static int lua_string_reverse(lua_State *Ls){
  *   Links:
  *   + http://luajit.org/ext_c_api.html
  *   + http://pgl.yoyo.org/luai/i/about
+ *   + https://en.wikipedia.org/wiki/UTF-8
  *
  *   Compilation:
  *   Check ./compile.sh
