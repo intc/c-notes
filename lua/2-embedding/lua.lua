0c1cb037282ce774a5a322aa1acf0db30b70deca
Antti Antinoja <antti@fennosys.fi>
2019-04-24 13:48:50 +0800

    updated

--- lua/2-embedding/lua.lua
+++ lua/2-embedding/lua.lua
@@ -1,10 +1,22 @@
-print("Printing with printf:")
+print([[
+This is a LUA program running.
 
-for n = 1, 10 do
-	local buffer=n.." sec\n"
+We call some C functions:
+
+* string_print()
+* sleep()
+* string_reverse()
+
+Let's go!
+]])
+
+for n = 1, 5 do
+	local buffer="Saippukauppias says: "..n.."€ for "..n.." kilograms of soap.\n"
+	--local buffer=n.." sec\n"
 	sleep(1000);
 	string_print(buffer)
-	io.write( string_reverse(buffer) )
+	print("And let's see the same reversed:")
+	io.write( string_reverse(buffer).."\n" )
 end
 
 
